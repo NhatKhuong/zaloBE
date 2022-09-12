c519c00802703246707f56b67f89f36633d42ad1
NGUYỄN THANH SƠN <nguyenthanhson162001@gmail.com>
2022-09-12 21:41:10 +0700

    update authoriration

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
-import express from "express";
-import dotenv from "dotenv"
-dotenv.config()
+import express, { Request, Response } from "express";
+import "express-async-errors";
+import "dotenv/config";
+import helmet from "helmet";
 import morgan from "morgan";
-import cors from "cors"
-import connectDB from "./model/connect";
-import route from "./route";
+import cors from "cors";
+import route from "./app/route";
 
-const app = express()
-app.use("/static", express.static("static"));
-app.use(cors())
-app.use(morgan("dev"))
-app.use(express.json())
-app.listen(parseInt(process.env.port as string))
+const port = Number(process.env.PORT || 5000);
+const app = express();
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+// Security
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+app.use("/api", route);
+// 404
+app.use("*", (req: Request, res: Response) =>
+  res.status(404).send("url not found")
+);
 
-connectDB();
-route(app)
+app.listen(port, () => {
+  console.log(`Express server started on http://localhost:${port}`);
+});
--- src/route/index.ts
+++
@@ -1,7 +0,0 @@
-import { Express } from "express";
-import userRouter from "./user";
-const route = (app: Express) => {
-    app.use("/user", userRouter);
-};
-
-export default route;
